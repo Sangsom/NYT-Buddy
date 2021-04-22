a63074cd90ff0666a57a5b16e0d3c5729739786c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-22 16:10:26 +0300

    Initial Commit

---
+++ NYT Buddy/NYT_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYT_BuddyApp.swift
+//  NYT Buddy
+//
+//  Created by Rinalds Domanovs on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct NYT_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
