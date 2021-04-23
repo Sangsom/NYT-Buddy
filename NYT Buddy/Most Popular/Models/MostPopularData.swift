22fccfee774f01619406c75f96796e5dd647cfd8
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-23 10:42:03 +0300

    Created initial models

---
+++ NYT Buddy/Most Popular/Models/MostPopularData.swift
@@ -0,0 +1,18 @@
+//
+//  MostPopularData.swift
+//  NYT Buddy
+//
+//  Created by Rinalds Domanovs on 23/04/2021.
+//
+
+import Foundation
+
+struct MostPopularData: Codable {
+    let status: String
+    let results: [MostPopularArticle]
+}
+
+struct MostPopularArticle: Codable, Identifiable {
+    var id: Int
+    var title: String
+}
