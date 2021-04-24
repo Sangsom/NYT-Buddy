a55211a9ce947199b2499fee29ce64bb6883db2a
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-24 06:15:32 +0300

    Showing loader view

--- NYT Buddy/Most Popular/Views/Most Popular/MostPopularView.swift
+++ NYT Buddy/Most Popular/Views/Most Popular/MostPopularView.swift
@@ -11,27 +11,35 @@ struct MostPopularView: View {
     @EnvironmentObject var mostPopularViewModel: MostPopularViewModel
 
     var body: some View {
-        VStack {
-            Picker("Period", selection: $mostPopularViewModel.selectedPeriod) {
-                ForEach(Period.allCases) { period in
-                    Text(period.stringValue)
-                }
-            }
-            .padding(.horizontal)
-            .padding(.bottom)
-            .pickerStyle(SegmentedPickerStyle())
-            .onChange(of: mostPopularViewModel.selectedPeriod, perform: { _ in
-                self.loadData()
-            })
-
-            ScrollView(.vertical, showsIndicators: true) {
-                VStack(alignment: .leading, spacing: 20.0) {
-                    ForEach(mostPopularViewModel.articles) { article in
-                        ArticleListItem(article: article)
+        ZStack {
+            VStack {
+                Picker("Period", selection: $mostPopularViewModel.selectedPeriod) {
+                    ForEach(Period.allCases) { period in
+                        Text(period.stringValue)
                     }
                 }
                 .padding(.horizontal)
-                .onAppear(perform: loadData)
+                .padding(.bottom)
+                .pickerStyle(SegmentedPickerStyle())
+                .onChange(of: mostPopularViewModel.selectedPeriod, perform: { _ in
+                    self.loadData()
+                })
+
+                ScrollView(.vertical, showsIndicators: true) {
+                    VStack(alignment: .leading, spacing: 20.0) {
+                        ForEach(mostPopularViewModel.articles) { article in
+                            ArticleListItem(article: article)
+                        }
+                    }
+                    .padding(.horizontal)
+                    .onAppear(perform: loadData)
+                }
+            }
+            .blur(radius: mostPopularViewModel.state == .loading ? 4 : 0)
+            .disabled(mostPopularViewModel.state == .loading)
+
+            if mostPopularViewModel.state == .loading {
+                LoadingView()
             }
         }
     }
