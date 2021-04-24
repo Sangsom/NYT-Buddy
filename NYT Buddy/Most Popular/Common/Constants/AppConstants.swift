f5f5fd1deaccd837593e223b3b44674906ec8f1d
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-24 05:48:11 +0300

    Created global app constants file and add API key

---
+++ NYT Buddy/Most Popular/Common/Constants/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  NYT Buddy
+//
+//  Created by Rinalds Domanovs on 24/04/2021.
+//
+
+import Foundation
+
+struct Constants {
+
+    struct API {
+        static let NYT_API_KEY = "10se5TvsUG7aG7rr31AxOcme0SEZOQ9q"
+    }
+
+}
