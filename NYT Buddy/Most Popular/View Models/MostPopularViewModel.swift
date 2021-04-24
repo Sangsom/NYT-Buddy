e9f1cb05b2c2f718ab447a3a0e96b39e848bbdcd
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-24 06:02:38 +0300

    Added LoadingState into view model

--- NYT Buddy/Most Popular/View Models/MostPopularViewModel.swift
+++ NYT Buddy/Most Popular/View Models/MostPopularViewModel.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
+enum LoadingState: Equatable {
+    case idle
+    case loading
+    case failed(NetworkError)
+    case success
+}
+
 class MostPopularViewModel: ObservableObject {
+    // MARK: - Properties
     var mostPopularDataService: MostPopularDataService?
 
     @Published var articles = [MostPopularArticle]()
@@ -16,20 +24,27 @@ class MostPopularViewModel: ObservableObject {
             mostPopularDataService?.updatePeriod(selectedPeriod)
         }
     }
+    @Published private(set) var state = LoadingState.idle
 
+    // MARK: - Init
     init() {
         mostPopularDataService = MostPopularDataService(period: selectedPeriod)
     }
 
+    // MARK: - Custom methods
     func fetchMostPopularArticles() {
+        state = .loading
+
         mostPopularDataService?.fetchMostViewedArticles { result in
             switch result {
             case .success(let articles):
                 DispatchQueue.main.async {
                     self.articles = articles
+                    self.state = .success
                 }
             case .failure(let error):
                 print("Failed to fetch currency data: \(error.localizedDescription)")
+                self.state = .failed(error)
             }
         }
     }
