a259f7192b306374b7a2e0a4eead8c62ac33e856
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-28 15:50:31 +0300

    Added tab view

--- NYT Buddy/NYT_BuddyApp.swift
+++ NYT Buddy/NYT_BuddyApp.swift
@@ -11,12 +11,27 @@ import SwiftUI
 struct NYT_BuddyApp: App {
     @StateObject var mostPopularViewModel = MostPopularViewModel()
 
+    @State private var selectedTab = 0
+
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                MostPopularView()
-                    .environmentObject(mostPopularViewModel)
-            }
+            TabView(selection: $selectedTab,
+                    content:  {
+                        NavigationView {
+                            MostPopularView()
+                                .environmentObject(mostPopularViewModel)
+                        }
+                        .tag(0)
+                        .tabItem {
+                            Image(systemName: "newspaper")
+                            Text("Popular Articles")
+                        }
+
+                        Text("Tab Content 2").tabItem {
+                            Image(systemName: "scroll")
+                            Text("Top Stories") }.tag(1)
+                    })
+
         }
     }
 }
