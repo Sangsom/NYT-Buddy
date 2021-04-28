c3c096433aba26210797b85ccebc848c9bfc928a
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-28 16:13:46 +0300

    Created Section enum

---
+++ NYT Buddy/Shared/Common/Enums/Section.swift
@@ -0,0 +1,37 @@
+//
+//  Section.swift
+//  NYT Buddy
+//
+//  Created by Rinalds Domanovs on 28/04/2021.
+//
+
+import Foundation
+
+enum Section: String {
+    case arts
+    case automobiles
+    case books
+    case business
+    case fashion
+    case food
+    case health
+    case home
+    case insider
+    case magazine
+    case movies
+    case nyregion
+    case obituaries
+    case opinion
+    case politics
+    case realestate
+    case science
+    case sports
+    case sundayreview
+    case technology
+    case theater
+    case tmagazine = "t-magazine"
+    case travel
+    case upshot
+    case us
+    case world
+}
