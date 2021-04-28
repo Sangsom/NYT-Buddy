25f3059a44f5282e14cc498db1de02ed064b9ce1
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-28 10:48:19 +0300

    Added subsection property

--- NYT Buddy/Most Popular/Models/MostPopularArticle.swift
+++ NYT Buddy/Most Popular/Models/MostPopularArticle.swift
@@ -15,6 +15,7 @@ struct MostPopularArticle: Codable, Identifiable, Hashable {
     var url: URL?
     var published: String
     var section: String
+    var subsection: String?
     var byline: String
     var media: [Media]
     var keywords: String
@@ -31,6 +32,7 @@ struct MostPopularArticle: Codable, Identifiable, Hashable {
         case url
         case published = "published_date"
         case section
+        case subsection
         case byline
         case media
         case keywords = "adx_keywords"
@@ -41,7 +43,7 @@ struct MostPopularArticle: Codable, Identifiable, Hashable {
 #if DEBUG
 extension MostPopularArticle {
     static let exampleData = [
-        MostPopularArticle(id: 100000007706389, title: "Johnson & Johnson Vaccinations Paused After Rare Clotting Cases Emerge", abstract: "Federal health officials called for a halt in the use of the company’s coronavirus vaccine while they study serious illnesses that developed in six American women.", published: "2021-04-25", section: "Business", byline: "By Rebecca Robbins", media: [Media.exampleData], keywords: "Coronavirus (2019-nCoV);Vaccination and Immunization;Drugs (Pharmaceuticals);Offit, Paul A;Centers for Disease Control and Prevention;Moderna Inc;Pfizer Inc;Walgreen Co"),
+        MostPopularArticle(id: 100000007706389, title: "Johnson & Johnson Vaccinations Paused After Rare Clotting Cases Emerge", abstract: "Federal health officials called for a halt in the use of the company’s coronavirus vaccine while they study serious illnesses that developed in six American women.", published: "2021-04-25", section: "World", subsection: "Europe", byline: "By Rebecca Robbins", media: [Media.exampleData], keywords: "Coronavirus (2019-nCoV);Vaccination and Immunization;Drugs (Pharmaceuticals);Offit, Paul A;Centers for Disease Control and Prevention;Moderna Inc;Pfizer Inc;Walgreen Co"),
         MostPopularArticle(id: 100000007682408, title: "Matt Gaetz Is Said to Face Justice Dept. Inquiry Over Sex With an Underage Girl", abstract: "An inquiry into the Florida congressman was opened in the final months of the Trump administration, people briefed on it said.", published: "2021-04-25", section: "Business", byline: "By Rebecca Robbins", media: [Media.exampleData], keywords: "Coronavirus (2019-nCoV);Vaccination and Immunization;Drugs (Pharmaceuticals);Offit, Paul A;Centers for Disease Control and Prevention;Moderna Inc;Pfizer Inc;Walgreen Co"),
         MostPopularArticle(id: 100000007694130, title: "There’s a Name for the Blah You’re Feeling: It’s Called Languishing", abstract: "The neglected middle child of mental health can dull your motivation and focus — and it may be the dominant emotion of 2021.", published: "2021-04-25", section: "Business", byline: "By Rebecca Robbins", media: [Media.exampleData], keywords: "Coronavirus (2019-nCoV);Vaccination and Immunization;Drugs (Pharmaceuticals);Offit, Paul A;Centers for Disease Control and Prevention;Moderna Inc;Pfizer Inc;Walgreen Co")
     ]
