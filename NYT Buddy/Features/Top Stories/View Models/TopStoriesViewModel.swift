6c11f263a7d4240b3b4d50b75471cbb04f4b330e
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-29 10:17:52 +0300

    Added initial properties and method

--- NYT Buddy/Features/Top Stories/View Models/TopStoriesViewModel.swift
+++ NYT Buddy/Features/Top Stories/View Models/TopStoriesViewModel.swift
@@ -7,4 +7,28 @@
 
 import Foundation
 
+class TopStoriesViewModel: ObservableObject {
+    // MARK: - Properties
+    var topStoriesDataService: TopStoriesDataService?
 
+    @Published var stories = [MostPopularArticle]()
+
+    // MARK: - Init
+    init() {
+        topStoriesDataService = TopStoriesDataService()
+    }
+
+    // MARK: - Custom methods
+    func fetchTopStories() {
+        topStoriesDataService?.fetchTopStories(for: .arts, completion: { result in
+            switch result {
+            case .success(let stories):
+                DispatchQueue.main.async {
+                    self.stories = stories
+                }
+            case .failure(let error):
+                print("Failed to fetch stroies data: \(error.localizedDescription)")
+            }
+        })
+    }
+}
