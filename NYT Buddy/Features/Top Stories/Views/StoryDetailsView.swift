31c3315f0ae2bc36d327ec6b243dd43520c8b473
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-30 11:55:42 +0300

    Created empty StoryDetailsView

---
+++ NYT Buddy/Features/Top Stories/Views/StoryDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  StoryDetailsView.swift
+//  NYT Buddy
+//
+//  Created by Rinalds Domanovs on 30/04/2021.
+//
+
+import SwiftUI
+
+struct StoryDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StoryDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoryDetailsView()
+    }
+}
