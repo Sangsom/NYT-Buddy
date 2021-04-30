11c5de903958a5566f86683e8cf3f64a10b23c1f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-30 12:06:31 +0300

    Created navigation links for story details views

--- NYT Buddy/Features/Top Stories/Views/StoriesScrollView.swift
+++ NYT Buddy/Features/Top Stories/Views/StoriesScrollView.swift
@@ -19,16 +19,20 @@ struct StoriesScrollView: View {
             ScrollView(.horizontal, showsIndicators: true, content: {
                 HStack(alignment: .top, spacing: 20.0) {
                     ForEach(stories.prefix(5), id: \.self) { story in
-                        VStack(spacing: 8) {
-                            if let multimedia = story.multimedia,
-                               let firstItem = multimedia.first {
-                                RemoteImage(url: firstItem.url.absoluteString, size: CGSize(width: 150, height: 150))
-                            }
-                            Text(story.title)
+                        NavigationLink(
+                            destination: StoryDetailsView(story: story),
+                            label: {
+                                VStack(spacing: 8) {
+                                    if let multimedia = story.multimedia,
+                                       let firstItem = multimedia.first {
+                                        RemoteImage(url: firstItem.url.absoluteString, size: CGSize(width: 150, height: 150))
+                                    }
+                                    Text(story.title)
 
-                            Spacer()
-                        }
-                        .frame(width: 150, height: 250)
+                                    Spacer()
+                                }
+                                .frame(width: 150, height: 250)
+                            })
                     }
                 }
             })
