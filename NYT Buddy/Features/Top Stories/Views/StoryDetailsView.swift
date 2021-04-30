50bb0090677c1abd93a517f94160d2d8a26ae7e4
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-04-30 12:06:08 +0300

    Created sample UI

--- NYT Buddy/Features/Top Stories/Views/StoryDetailsView.swift
+++ NYT Buddy/Features/Top Stories/Views/StoryDetailsView.swift
@@ -16,10 +16,55 @@ struct StoryDetailsView: View {
 
     // MARK: - Body
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 10.0) {
+            HStack(alignment: .center) {
+                Text(story.section.capitalized)
+                    .bold()
+                    .underline(true, color: Color("UnderlineColor"))
+
+                if let subsection = story.subsection,
+                   !subsection.isEmpty {
+                    Image(systemName: "arrow.right")
+                    Text(subsection)
+                        .bold()
+                        .underline(true, color: .red)
+                }
+
+                Spacer()
+                Text(story.published)
+            }
+            .padding(.top)
+            .foregroundColor(Color("AccentColor"))
+
+            Text(story.title)
+                .font(.title)
+                .fontWeight(.medium)
+
+            Text(story.abstract)
+                .font(.body)
+
+            HStack(alignment: .center) {
+                Text(story.byline)
+                    .foregroundColor(.secondary)
+                Spacer()
+                Button(action: openFullStory) {
+                    Text("Full story")
+                }
+            }
+
+//            MediaView(media: story.multimedia)
+
+            Spacer()
+        }
+        .navigationBarTitle("", displayMode: .inline)
+        .padding(.horizontal)
     }
 
     // MARK: - Custom methods
+    func openFullStory() {
+        guard let url = story.url else { return }
+        openURL(url)
+    }
 }
 
 // MARK: - Preview
