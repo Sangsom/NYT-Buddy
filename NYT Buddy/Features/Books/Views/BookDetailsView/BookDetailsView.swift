3e59e255f9ebb194a9af399dd283160c45eb307c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-05-10 14:53:51 +0300

    Added some book details

--- NYT Buddy/Features/Books/Views/BookDetailsView/BookDetailsView.swift
+++ NYT Buddy/Features/Books/Views/BookDetailsView/BookDetailsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MobileCoreServices
 
 struct BookDetailsView: View {
     // MARK: - Properties
@@ -13,18 +14,56 @@ struct BookDetailsView: View {
 
     // MARK: - Body
     var body: some View {
-        GeometryReader { geo in
+        ScrollView(.vertical, showsIndicators: false) {
             VStack(alignment: .leading, spacing: 10.0, content: {
                 if let imageUrl = book.bookImage {
+                    HStack {
+                        Spacer()
                         RemoteImage(
-                            url: imageUrl,
-                            size: CGSize(width: geo.size.width, height: geo.size.height / 3),
-                            aspectRatio: .fit)
+                                url: imageUrl,
+                                size: nil,
+                                aspectRatio: .fit)
+                            .frame(maxHeight: 300)
+                        Spacer()
                     }
+                    .padding(.top)
+                }
+
                 Text(book.title)
+                    .font(.title)
+                    .fontWeight(.medium)
+
+                Text(book.description)
+                    .font(.body)
+
+                HStack(alignment: .center) {
+                    Text("#\(book.rank)")
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color("AccentColor"))
+
+                    Spacer()
+                    Text("by \(book.author)")
+                        .font(.headline)
+                        .fontWeight(.medium)
+                        .foregroundColor(.secondary)
+                }
+
+                HStack {
+                    Text("ISBN: \(book.isbn13)")
+                    Spacer()
+                    Button(action: {
+                        UIPasteboard.general.setValue(book.isbn13, forPasteboardType: kUTTypePlainText as String)
+                    }, label: {
+                        Image(systemName: "doc.on.doc.fill")
+                            .foregroundColor(.primary)
+                    })
+                }
 
                 Spacer()
             })
+            .navigationBarTitle("", displayMode: .inline)
+            .padding(.horizontal)
         }
     }
 }
