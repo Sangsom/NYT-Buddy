fb2b442f82a5b0885eeafe520e238fe9a217a76c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-05-13 09:59:20 +0300

    Created empty BooksDataServiceTests

---
+++ Asynchronous NYT BuddyTests/BooksDataServiceTests.swift
@@ -0,0 +1,14 @@
+//
+//  BooksDataServiceTests.swift
+//  Asynchronous NYT BuddyTests
+//
+//  Created by Rinalds Domanovs on 13/05/2021.
+//
+
+import XCTest
+
+@testable import NYT_Buddy
+
+class BooksDataServiceTests: XCTestCase {
+
+}
