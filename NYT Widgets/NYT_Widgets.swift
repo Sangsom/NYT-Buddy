3062ccd77cdbf6e9df3aaf99093a31ea9d3d823a
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2021-05-20 16:35:21 +0300

    Added period enum intent

--- NYT Widgets/NYT_Widgets.swift
+++ NYT Widgets/NYT_Widgets.swift
@@ -13,19 +13,22 @@ struct NYTEntry: TimelineEntry {
     let article: MostPopularArticle
 }
 
-struct Provider: TimelineProvider {
+struct Provider: IntentTimelineProvider {
+    typealias Entry = NYTEntry
+    typealias Intent = SelectMostPopularPeriodIntent
+
     var mostPopularDataService = MostPopularDataService(period: Period.daily.rawValue)
 
     func placeholder(in context: Context) -> NYTEntry {
         NYTEntry(date: Date(), article: MostPopularArticle.exampleData.first!)
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (NYTEntry) -> Void) {
+    func getSnapshot(for configuration: SelectMostPopularPeriodIntent, in context: Context, completion: @escaping (NYTEntry) -> Void) {
         let entry = NYTEntry(date: Date(), article: MostPopularArticle.exampleData.first!)
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<NYTEntry>) -> Void) {
+    func getTimeline(for configuration: SelectMostPopularPeriodIntent, in context: Context, completion: @escaping (Timeline<NYTEntry>) -> Void) {
         mostPopularDataService.fetchMostPopularArticles(for: .viewed) { result in
             switch result {
             case .success(let articles):
@@ -56,8 +59,9 @@ struct NYT_Widgets: Widget {
     let kind: String = "NYT_Widgets"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(
+        IntentConfiguration(
             kind: kind,
+            intent: SelectMostPopularPeriodIntent.self,
             provider: Provider()
         ) { entry in
             WidgetEntryView(entry: entry)
